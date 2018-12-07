05c3e589b79935f5571cf74d1440be35abb535ae
gitVictor <victormarzac@yahoo.com>
2018-12-07 09:53:28 +0200

    Initial commit

---
+++ src/Problem_5.java
@@ -0,0 +1,23 @@
+public class Problem_5 {
+    public static void main(String[] args){
+        long num = 600851475143L;
+        long largest = 0;
+        for(long i = 3; i * i < num; i += 2) {
+            if(num % i == 0 && isPrime(i)){
+                if(i > largest) {
+                    largest = i;
+                }
+            }
+        }
+        System.out.println(largest);
+    }
+
+    public static boolean isPrime(long n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if (n % i == 0 && n != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
